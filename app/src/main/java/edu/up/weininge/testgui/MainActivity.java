f7210bd490cd46397bb57e08640f7c469c86109f
Alex <alex.weininger@live.com>
2018-09-20 23:31:30 -0700

    added external citation hid build menu show scoreboard and dev card menus

--- app/src/main/java/edu/up/weininge/testgui/MainActivity.java
+++ app/src/main/java/edu/up/weininge/testgui/MainActivity.java
@@ -9,8 +9,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.SurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.HeaderViewListAdapter;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
@@ -18,40 +20,41 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Remove title bar
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-//Remove notification bar
+        /**
+         External Citation
+         Date: 20 September 2018
+         Problem: Needed more screen space and wanted to get rid of the title bar and the
+         notification bar
+         Resource:
+         https://stackoverflow.com/questions/2591036/how-to-hide-the-title-bar-for-an-activity-in-xml-with-existing-custom-theme
+         Solution: I used the code from the stackoverflow post.
+         */
+
+        // remove title bar and notification bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // boardSurfaceView board is the custom SurfaceView
         boardSurfaceView board = (boardSurfaceView) findViewById(R.id.board);
 
+        // create Canvas object
         Canvas canvas = new Canvas();
 
-//        Button buildMenuButton
-
+        // draw the board of hexagons and ports on the canvas
         board.createHexagons();
         board.createPorts();
-
-
         board.draw(canvas);
 
-
-
-
-        /*
-        need to create listeners for every single button
-         */
-
-        //Spinner
-        /*
+        // the spinner that holds the development cards the player has
         Spinner developmentSpinner = (Spinner) findViewById(R.id.development_Card_Spinner);
-        ArrayAdapter<CharSequence> developmentChoices = ArrayAdapter.createFromResource(this,R.array.resource_Card, android.R.layout.simple_spinner_item);
-        developmentChoices.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        developmentSpinner.setAdapter(developmentChoices);*/
 
+        // spinner logic and handlers
+        ArrayAdapter<CharSequence> developmentChoices = ArrayAdapter.createFromResource(this, R.array.resource_Card, android.R.layout.simple_spinner_item);
+        developmentChoices.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        developmentSpinner.setAdapter(developmentChoices);
     }
 }
